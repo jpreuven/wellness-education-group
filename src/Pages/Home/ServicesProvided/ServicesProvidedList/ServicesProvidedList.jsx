1dd0acb82c5243ac2212fe52413571d574e62f98
Jonathan <jpreuven@gmail.com>
2024-01-07 15:37:14 -0800

    Added styled components to services provided

--- src/Pages/Home/ServicesProvided/ServicesProvidedList/ServicesProvidedList.jsx
+++ src/Pages/Home/ServicesProvided/ServicesProvidedList/ServicesProvidedList.jsx
@@ -3,59 +3,42 @@ import { Box } from "@mui/system";
 import React from "react";
 import theme from "../../../../theme/theme";
 import { servicesProvidedListData } from "./ServicesProvidedList.data";
+import {
+  StyledServicesProvidedListOuterGrid,
+  StyledServicesProvidedListInnerGrid,
+  StyledServicesProvidedListLogoGrid,
+  StyledServicesProvidedListLogoTypography,
+} from "./ServicesProvidedList.styles";
 
 export default function ServicesProvidedList() {
   const servicesProvidedList = servicesProvidedListData.map(
     ({ logo, title, caption }, index) => {
       return (
-        <Grid
+        <StyledServicesProvidedListInnerGrid
           item
           key={index}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "center",
-            gap: "0.5rem",
-          }}
           xs={12}
           sm={5}
           md={4}
           lg={2}
         >
-          <Grid
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              sx={{
-                background: `linear-gradient(to top, ${theme.palette.primary.main},${theme.palette.secondary.main})`,
-                borderRadius: "50%",
-                width: "125px",
-                height: "125px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                boxShadow: "0px 5px 5px gray",
-              }}
-            >
+          <StyledServicesProvidedListLogoGrid>
+            <StyledServicesProvidedListLogoTypography>
               {logo}
-            </Typography>
-          </Grid>
+            </StyledServicesProvidedListLogoTypography>
+          </StyledServicesProvidedListLogoGrid>
           <Typography variant="h6" component="h6">
             {title}
           </Typography>
           <Typography>{caption}</Typography>
-        </Grid>
+        </StyledServicesProvidedListInnerGrid>
       );
     }
   );
 
   return (
-    <Grid container sx={{ display: "flex", justifyContent: "space-evenly" }}>
+    <StyledServicesProvidedListOuterGrid container>
       {servicesProvidedList}
-    </Grid>
+    </StyledServicesProvidedListOuterGrid>
   );
 }
---
+++ src/Pages/Home/ServicesProvided/ServicesProvidedList/ServicesProvidedList.styles.jsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+import { Grid, Typography } from "@mui/material";
+
+export const StyledServicesProvidedListOuterGrid = styled(Grid)(
+  ({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+  })
+);
+
+export const StyledServicesProvidedListInnerGrid = styled(Grid)(
+  ({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+    gap: "0.5rem",
+  })
+);
+
+export const StyledServicesProvidedListLogoGrid = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+export const StyledServicesProvidedListLogoTypography = styled(Typography)(
+  ({ theme }) => ({
+    background: `linear-gradient(to top, ${theme.palette.primary.main},${theme.palette.secondary.main})`,
+    borderRadius: "50%",
+    width: "125px",
+    height: "125px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "0px 5px 5px gray",
+  })
+);

140dbc32000fe24538d0f86419e6bd95c7d8c217
Jonathan <jpreuven@gmail.com>
2024-01-10 14:52:04 -0800

    updated services provided content

--- src/Pages/Home/ServicesProvided/ServicesProvidedList/ServicesProvidedList.data.jsx
+++ src/Pages/Home/ServicesProvided/ServicesProvidedList/ServicesProvidedList.data.jsx
@@ -4,19 +4,22 @@ function schoolLogo() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-school"
+      class="icon icon-tabler icon-tabler-book"
       width="100"
       height="100"
       viewBox="0 0 24 24"
-      strokeWidth="1.1"
+      stroke-width=".75"
       stroke={theme.palette.primary.tan}
       fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
-      <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+      <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+      <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+      <path d="M3 6l0 13" />
+      <path d="M12 6l0 13" />
+      <path d="M21 6l0 13" />
     </svg>
   );
 }
@@ -62,6 +65,26 @@ function heartLogo() {
     </svg>
   );
 }
+function heartLogo2() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon icon-tabler icon-tabler-hearts"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+      stroke-width=".75"
+      stroke={theme.palette.primary.tan}
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M14.017 18l-2.017 2l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 0 1 8.153 5.784" />
+      <path d="M15.99 20l4.197 -4.223a2.81 2.81 0 0 0 0 -3.948a2.747 2.747 0 0 0 -3.91 -.007l-.28 .282l-.279 -.283a2.747 2.747 0 0 0 -3.91 -.007a2.81 2.81 0 0 0 -.007 3.948l4.182 4.238z" />
+    </svg>
+  );
+}
 
 function smileLogo() {
   return (
@@ -82,6 +105,43 @@ function smileLogo() {
       <path d="M9 10l.01 0" />
       <path d="M15 10l.01 0" />
       <path d="M9.5 15a3.5 3.5 0 0 0 5 0" />
+    </svg>
+  );
+}
+
+function telephoneLogo() {
+  return (
+    // <svg
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   class="icon icon-tabler icon-tabler-phone"
+    //   width="95"
+    //   height="95"
+    //   viewBox="0 0 24 24"
+    //   stroke-width="0.75"
+    //   stroke={theme.palette.primary.tan}
+    //   fill="none"
+    //   stroke-linecap="round"
+    //   stroke-linejoin="round"
+    // >
+    //   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    //   <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+    // </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon icon-tabler icon-tabler-phone-call"
+      width="95"
+      height="95"
+      viewBox="0 0 24 24"
+      stroke-width=".75"
+      stroke={theme.palette.primary.tan}
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+      <path d="M15 7a2 2 0 0 1 2 2" />
+      <path d="M15 3a6 6 0 0 1 6 6" />
     </svg>
   );
 }
@@ -112,31 +172,26 @@ const servicesProvidedListData = [
   {
     logo: schoolLogo(),
     title: "School",
-    caption: `We provide at school services. We provide at school services`,
+    caption: `We work together with schools to get your child the best care possible.`,
   },
   {
     logo: lightBulbLogo(),
-    title: "Therapy",
+    // title: "Counseling for all ages",
+    title: "All Ages",
     caption:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, dolores!",
+      "We provide counseling for people of all ages, starting from as low as two years old.",
   },
   {
-    logo: heartLogo(),
-    title: "Specialized Care",
+    logo: telephoneLogo(),
+    title: "Telehealth",
     caption:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, ullam.",
+      "Can't meet in person? No problem! We offer counseling over the phone and zoom!",
   },
   {
-    logo: smileLogo(),
-    title: "Happiness",
+    logo: heartLogo2(),
+    title: "Marriage and Parenting",
     caption:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, laborum.",
-  },
-  {
-    logo: humanLogo(),
-    title: "Health",
-    caption:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, eveniet!",
+      "We provide parents and couples with the necessary tools to build stronger relationships.",
   },
 ];
 
